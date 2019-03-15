65d0d04ac1ab9e63211545fae20aa3f62d2bf89e
wakataw <agungpratama1001@gmail.com>
2019-03-15 09:48:26 +0700

    menambahkan modul detil pengumuman paket tender

--- pypro/lpse.py
+++ pypro/lpse.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod, ABCMeta
+
 import requests
 import re
 
@@ -68,6 +70,9 @@ class Lpse(object):
         :param nama_penyedia: filter berdasarkan nama penyedia
         :return: dictionary dari hasil pencarian paket (atau list jika data_only=True)
         """
+
+        # TODO: Header dari data berbeda untuk tiap SPSE masing-masing ILAP. Cek tiap LPSE tiap ilap untuk menentukan header dari data
+
         params = {
             'draw': 1,
             'start': start,
@@ -127,6 +132,103 @@ class Lpse(object):
         """
         return self.get_paket('pl', start, length, data_only, kategori, search_keyword)
 
+    def get_detil(self, id_paket):
+        return LpseDetil(self, id_paket)
+
     def __del__(self):
         self.session.close()
         del self.session
+
+
+class LpseDetil(object):
+
+    def __init__(self, lpse, id_paket):
+        self._lpse = lpse
+        self.id_paket = id_paket
+        self.get_detil()
+
+    def get_detil(self):
+        self.pengumuman = LpseDetilPengumumanParser(self._lpse, self.id_paket).get_detil()
+
+    def __str__(self):
+        return str(self.todict())
+
+    def todict(self):
+        data = self.__dict__
+        data.pop('_lpse')
+        return data
+
+
+class BaseLpseDetilParser(object):
+
+    def __init__(self, lpse, detil_path):
+        self.lpse = lpse
+        self.detil_path = detil_path
+
+    def get_detil(self):
+        r = self.lpse.session.get(self.lpse.host+self.detil_path)
+        return self._parse_detil(r.content)
+
+    @abstractmethod
+    def _parse_detil(self, content):
+        pass
+
+
+class LpseDetilPengumumanParser(BaseLpseDetilParser):
+
+    def __init__(self, lpse, id_paket):
+        super().__init__(lpse, '/lelang/{}/pengumumanlelang'.format(id_paket))
+
+    def _parse_detil(self, content):
+        soup = Bs(content, 'html5lib')
+
+        content = soup.find('div', {'class': 'content'})
+        table = content.find('table', {'class': 'table-bordered'}).find('tbody')
+
+        return self._parse_table(table)
+
+    def _parse_table(self, table):
+        data = {}
+
+        for tr in table.find_all('tr', recursive=False):
+            ths = tr.find_all('th', recursive=False)
+            tds = tr.find_all('td', recursive=False)
+
+            for th, td in zip(ths, tds):
+                data_key = '_'.join(th.text.strip().split()).lower()
+
+                td_sub_table = td.find('table', recursive=False)
+
+                if td_sub_table and data_key == 'rencana_umum_pengadaan':
+                    data_value = self._parse_rup(td_sub_table.find('tbody'))
+                elif data_key == 'syarat_kualifikasi':
+                    # TODO: Buat parser syarat kualifikasi, tapi perlu tahu dulu kemungkinan format dan isinya
+                    continue
+                elif data_key == 'lokasi_pekerjaan':
+                    data_value = self._parse_lokasi_pekerjaan(td)
+                else:
+                    data_value = ' '.join(td.text.strip().split())
+
+                data.update({
+                    data_key: data_value
+                })
+
+        return data
+
+    def _parse_rup(self, tbody_rup):
+        raw_data = []
+        for tr in tbody_rup.find_all('tr', recursive=False):
+            raw_data.append([' '.join(i.text.strip().split()) for i in tr.children])
+
+        header = ['_'.join(i.split()).lower() for i in raw_data[0]]
+        data = {}
+
+        for row in raw_data[1:]:
+            data.update(zip(header, row))
+
+        data.pop('')
+
+        return data
+
+    def _parse_lokasi_pekerjaan(self, td_pekerjaan):
+        return [' '.join(li.text.strip().split()) for li in td_pekerjaan.find_all('li')]
