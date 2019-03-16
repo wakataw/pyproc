775c073517cda25b11f6ef41b07ccd1d0cc2a97f
wakataw <agungpratama1001@gmail.com>
2019-03-16 23:11:15 +0700

    list exported  members for `import *`

--- pyproc/__init__.py
+++ pyproc/__init__.py
@@ -1,2 +1,13 @@
+from .kategori import *
+from .lpse import Lpse
+
 __version__ = '0.1.0a'
 __author__ = 'Agung Pratama'
+__all__ = [
+    'PENGADAAN_BARANG',
+    'PEKERJAAN_KONSTRUKSI',
+    'JASA_KONSULTANSI',
+    'JASA_KONSULTANSI_PERORANGAN',
+    'JASA_LAINNYA',
+    'Lpse'
+]
