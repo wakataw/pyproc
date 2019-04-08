57de402bfb84e4ceb978bffa931c1c3a469dc49d
wakataw <agungpratama1001@gmail.com>
2019-04-08 21:51:25 +0700

    clean `[...]` dari kolom tahap_tender_saat_ini

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -79,7 +79,9 @@ def combine_data():
             detil.update((k, data['pengumuman'][k]) for k in detil.keys() & data['pengumuman'].keys())
             if data['pemenang']:
                 detil.update((k, data['pemenang'][k]) for k in detil.keys() & data['pemenang'].keys())
+
             detil['lokasi_pekerjaan'] = ', '.join(detil['lokasi_pekerjaan'])
+            detil['tahap_tender_saat_ini'] = detil['tahap_tender_saat_ini'].strip(r' [...]')
 
             writer.writerow(detil)
 
