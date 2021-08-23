1fadd80191b8ce92d71d5170a898b104a418f4f9
wakataw <agungpratama1001@gmail.com>
2021-08-23 15:55:01 +0700

    remove bump.py

--- pyproc/__init__.py
+++ pyproc/__init__.py
@@ -1,7 +1,7 @@
 from .kategori import *
 from .lpse import Lpse, JenisPengadaan
 
-__version__ = '0.1.9a'
+__version__ = '0.1.9a1'
 __author__ = 'Agung Pratama'
 __all__ = [
     'PENGADAAN_BARANG',
