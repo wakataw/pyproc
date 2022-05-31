bdf49e54f6aa4378c39d99653adbbb9b1e13d84f
wakataw <pratamaagung@hotmail.com>
2022-05-31 18:03:03 +0700

    fix lpse list downloader

--- pyproc/utils.py
+++ pyproc/utils.py
@@ -26,17 +26,35 @@ def get_all_host():
 
 def download_host(logging, name='daftarlpse.csv'):
     data = get_all_host()
+    hosts = dict()
+    invalid_host = 0
 
-    logging.info("{} alamat LPSE ditemukan".format(len(data)))
+    for item in data:
+        try:
+            url = item['repo_url4']
+        except KeyError:
+            url = item['repo_url']
+
+        parsed_url = urlparse(url)
+
+        if not parsed_url.scheme.startswith('http'):
+            invalid_host += 1
+            continue
+
+        hosts[url] = str(item['repo_id']) + '-' + \
+            ' '.join([i for i in re.sub(r'[^a-zA-Z\d\s]', ' ', item['repo_nama']).split() if i.strip() != ''])
+
+    logging.info(
+        "{} alamat LPSE ditemukan. {} alamat valid, {} alamat tidak valid, {} alamat terduplikasi.".format(
+            len(data), len(hosts), invalid_host, len(data) - len(hosts) - invalid_host
+        )
+    )
+    logging.debug(hosts)
 
     with open(name, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter=';')
-        for item in data:
-            try:
-                url = item['repo_url4']
-            except KeyError:
-                url = item['repo_url']
-            writer.writerow([url, f"{item['repo_id']}-{item['repo_nama']}"])
+        for k, v in hosts.items():
+            writer.writerow([k, v])
 
     logging.info("Export daftar lpse ke {}".format(name))
 
